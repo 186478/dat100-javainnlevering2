e42cebcabb2cc4c2ca9a541d289e71689cd1198b
Teodor <teodor.orjansen@gmail.com>
2024-09-24 15:34:33 +0200

    ferdig tabell.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -4,40 +4,62 @@ public class Tabeller {
 
 	public static void main(String[] args) {
 		int[] heltallTabell = { 3, 7, 2, 8 };
+		System.out.print("skrivUt: ");
 		skrivUt(heltallTabell);
+		
+		System.out.println("");
 
 		int[] strengTabell = { 42, 67, 89 };
 		String resultat = tilStreng(strengTabell);
+		System.out.print("tilStreng: ");
 		System.out.println(resultat);
+		
+		int[] sumTabell = {20, 13, 30};
+		int sum = summer(sumTabell);
+		System.out.print("summer: ");
+		System.out.print(sum);
+		
+		System.out.println("");
 		
 		int[] sjekkTabell = {1, 2, 4, 6, 10, 14};
 		boolean sjekk = finnesTall(sjekkTabell, 2);
+		System.out.print("finnesTall: ");
 		System.out.println(sjekk);
 
 		int[] posisjonTabell = { 1, 2, 3, 4, 5, 6, 7 };
-		// int posisjon = posisjonTall(posisjonTabell, 3);
-		// System.out.println("Posisjonen til tallet er: " + posisjon);
-		// posisjon = posisjonTall(posisjonTabell, 60);
-		// System.out.println(posisjon);
+		int posisjon = posisjonTall(posisjonTabell, 3);
+		System.out.print("posisjonTall: ");
+		System.out.println("Posisjonen til tallet er: " + posisjon);
+		posisjon = posisjonTall(posisjonTabell, 60);
+		System.out.print("feil posisjonTall: ");
+		System.out.println(posisjon);
 		
 		
 		int [] normalTabell = {1,2,3,4,5,6};
 		int [] reversertTabell = reverser(normalTabell);
-		//for (int tall : reversertTabell) {
-        //    System.out.print(tall + " ");
-        //}
+		System.out.print("reverser: ");
+		for (int tall : reversertTabell) {
+            System.out.print(tall + " ");
+        }
+		
+		System.out.println("");
+		
+		int[] usortertTabell = {5, 3, 2, 6, 1};
+        boolean sjekket = erSortert(usortertTabell);
+        System.out.print("erSortert: ");
+        System.out.println(sjekket);
 		
 		
 		int[] tabell1 = {1, 2, 3};
         int[] tabell2 = {4, 5, 6};
         int[] sammensattTabell = settSammen(tabell1, tabell2);
+        System.out.print("settSammen: ");
         for (int tall : sammensattTabell) {
             System.out.print(tall + " ");
         }
+       
         
-        int[] usortertTabell = {5, 3, 2, 6, 1};
-        boolean sjekket = erSortert(usortertTabell);
-        System.out.println(sjekket);
+        
 
 	}
 
