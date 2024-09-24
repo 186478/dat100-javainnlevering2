5919f17565b918f7150b7548a043d23a29a33b1d
Teodor <teodor.orjansen@gmail.com>
2024-09-24 14:04:46 +0200

    oppgave b

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -3,23 +3,41 @@ package no.hvl.dat100.tabeller;
 public class Tabeller {
 
 	public static void main(String[] args) {
-		int [] tabell = {3,7,2,8};
-		skrivUt(tabell);
-
+		int [] heltallTabell = {3,7,2,8};
+		//skrivUt(heltallTabell);
+		
+		int [] strengTabell = {42,67,89};
+		String resultat = tilStreng(strengTabell);
+		System.out.println(resultat);
 	}
 
 	public static void skrivUt(int[] tabell) {
 		
 		for (int i = 0; i < tabell.length; i++) {
-			System.out.print(tabell[i] + ", ");
+			System.out.print(tabell[i]);
+			if (i < tabell.length - 1) {
+	            System.out.print(", ");
+	        }
 		}
+		
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		String resultat = "[";
+		
+		for (int i = 0; i < tabell.length; i++) {
+			resultat += tabell[i];
+			
+			if (i < tabell.length -1) {
+				resultat += ",";
+			}
+		}
+		
+		resultat += "]";
+		
+		return resultat;
 	}
 
 	// c)
@@ -40,8 +58,8 @@ public class Tabeller {
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
+		
+		
 	}
 
 	// f)
