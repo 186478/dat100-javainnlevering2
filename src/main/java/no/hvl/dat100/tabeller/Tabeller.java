e0f2eb67a6375bc946689b5f72bc90744a1ddd3f
186512 <148553915+186512@users.noreply.github.com>
2024-09-24 14:49:40 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -3,60 +3,55 @@ package no.hvl.dat100.tabeller;
 public class Tabeller {
 
 	public static void main(String[] args) {
-		int [] heltallTabell = {3,7,2,8};
+		int[] heltallTabell = { 3, 7, 2, 8 };
 		skrivUt(heltallTabell);
-		
-		int [] strengTabell = {42,67,89};
+
+		int[] strengTabell = { 42, 67, 89 };
 
 		String resultat = tilStreng(strengTabell);
 		System.out.println(resultat);
-		
-		int[] sjekkTabell = {1, 2, 4, 6, 10, 14};
-		boolean sjekk = finnesTall(sjekkTabell, );
-		System.out.println(sjekk);
-		
-		
-		
-		 
-		
 
-		//String resultat = tilStreng(strengTabell);		
-		//System.out.print(resultat);
-		
-		int [] posisjonTabell = {1,2,3,4,5,6,7};
-		//int posisjon = posisjonTall(posisjonTabell, 3);
-		//System.out.println("Posisjonen til tallet er: " + posisjon);
-		//posisjon = posisjonTall(posisjonTabell, 60);
-        //System.out.println(posisjon);
+		int[] sjekkTabell = { 1, 2, 4, 6, 10, 14 };
+		boolean sjekk = finnesTall(sjekkTabell, 7);
+		System.out.println(sjekk);
+
+		// String resultat = tilStreng(strengTabell);
+		// System.out.print(resultat);
+
+		int[] posisjonTabell = { 1, 2, 3, 4, 5, 6, 7 };
+		// int posisjon = posisjonTall(posisjonTabell, 3);
+		// System.out.println("Posisjonen til tallet er: " + posisjon);
+		// posisjon = posisjonTall(posisjonTabell, 60);
+		// System.out.println(posisjon);
 
 	}
 
 	public static void skrivUt(int[] tabell) {
-		
+
 		for (int i = 0; i < tabell.length; i++) {
 			System.out.print(tabell[i]);
 			if (i < tabell.length - 1) {
-	            System.out.print(", ");
-	        }
+				System.out.print(", ");
+			}
 		}
-		
-	} 
+
+	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
 		String resultat = "[";
-		
+
 		for (int i = 0; i < tabell.length; i++) {
 			resultat += tabell[i];
-			
-			if (i < tabell.length -1) {
+
+			if (i < tabell.length - 1) {
 				resultat += ",";
 			}
 		}
-		
+
 		resultat += "]";
-		
+
 		return resultat;
 	}
 
@@ -68,20 +63,17 @@ public class Tabeller {
 	}
 
 	// d)
-	public static boolean finnesTall(int[] Tabell, int tall ) {
-		
-		
-		
+	public static boolean finnesTall(int[] tabell, int tall) {
 
-		for (int i = 0; i <= Tabell.length; i++) {
-		     if (Tabell[i] == tall) {
-	                return true;
-	            }
-		    }
-	        return false;
-	    
+		for (int i = 0; i < tabell.length; i++) {
+			if (tabell[i] == tall) {
+				return true;
+			}
+		}
+
+		return false;
 	}
-	
+
 	
 
 	// e)
@@ -93,7 +85,7 @@ public class Tabeller {
 			}
 		}
 		return -1;
-		
+
 	}
 
 	// f)
