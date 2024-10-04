92f83c08c605677826f92b3efa6f10d9f9cd4ff1
Teodor <teodor.orjansen@gmail.com>
2024-10-04 12:24:16 +0200

    Update Matriser.java

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -19,20 +19,10 @@ public class Matriser {
 	}
 
 	// b) konvertere int til string
-	public static String tilStreng(String[][] matrise) {
-		StringBuilder sb = new StringBuilder();
-		for (int rad = 0; rad < matrise.length; rad++) {
-			for (int kol = 0; kol < matrise[rad].length; kol++) {
-				sb.append(matrise[rad][kol]);
-				if (kol < matrise[rad].length - 1) {
-					sb.append(", ");
-				}
-			}
-			if (rad < matrise.length - 1) {
-				sb.append("\n"); // Linjeskift etter hver rad
-			}
-		}
-		return sb.toString();
+	public static String tilStreng(int[][] matrise) {
+		
+		
+		
 	}
 
 	// c)
